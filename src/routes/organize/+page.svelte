1c36b6a843b927eb3be339835d66f39626bf56b2
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-04-24 02:16:41 +0530

    changes in main & form Page

--- src/routes/organize/+page.svelte
+++ src/routes/organize/+page.svelte
@@ -13,6 +13,16 @@
     ContactNo: "",
     Message: "",
   };
+  $: if (eventDetails.ContactNo.length > 10) {
+    eventDetails.ContactNo = eventDetails.ContactNo.slice(0, 10);
+  }
+  $: if (eventDetails.Budget > 1000000) {
+    eventDetails.Budget = 1000000;
+  }
+  $: if (eventDetails.first_name.length > 15) {
+    eventDetails.first_name = eventDetails.first_name.slice(0, 20);
+  }
+
   let cnt = 0;
   let handleData = async () => {
     cnt = 1;
@@ -41,16 +51,22 @@
 <div class=" py-6 sm:py-8 lg:py-12 bg-white">
   <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
     <!-- text - start -->
-    <div class="mb-10 md:mb-16">
+    <div class="mb-10 md:mb-5">
       <h2
-        class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl"
+        class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-4xl"
       >
-        Get in touch
+        Organize an event with us Today
+        <br />
+        <span class="text-sky-500"> Get In Touch </span>
       </h2>
 
       <div class="text-center flex justify-center align-middle items-center">
-        <a class="text-indigo-500 mx-5">eventhub@email.com</a>
-        <p class="leading-normal my-5">Phone no: 1234567890</p>
+        <button class="btn gap-2 lowercase bg-transparent text-gray-900 border-sky-500 mx-4 hover:bg-sky-300">
+          eventhub@email.com
+        </button>
+        <button class="btn gap-2 lowercase bg-transparent text-gray-900 border-sky-500 mx-4 hover:bg-sky-300">
+          Phone no: 9137893440
+        </button>
       </div>
     </div>
     <!-- text - end -->
@@ -67,10 +83,12 @@
           >First name*</label
         >
         <input
+          placeholder="First Name"
+          required
           name="first-name"
           type="text"
           bind:value={eventDetails.first_name}
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-sky-300 transition duration-100 focus:ring"
         />
       </div>
 
@@ -81,10 +99,12 @@
           >Last name*</label
         >
         <input
+          required
+          placeholder="Last Name"
           name="last-name"
           type="text"
           bind:value={eventDetails.last_name}
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
         />
       </div>
 
@@ -98,9 +118,9 @@
           name="event"
           id="location"
           bind:value={eventDetails.Event_type}
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
         >
-          <option value="1">Pick your event</option>
+          <option value="Pick Your Event" selected>Pick your event</option>
           <option value="Wedding">Wedding</option>
           <option value="Anniversary">Anniversary</option>
           <option value="Party">Party</option>
@@ -117,7 +137,7 @@
         >
         <input
           name="no of guest"
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
           type="number"
           bind:value={eventDetails.NoOfGuest}
         />
@@ -134,7 +154,7 @@
           name="location"
           id="location"
           bind:value={eventDetails.Location}
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
         >
           <option value="1">Pick your location</option>
           <option value="Mumbai">Mumbai</option>
@@ -155,7 +175,7 @@
         >
         <input
           name="Date"
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
           type="date"
           bind:value={eventDetails.Date}
         />
@@ -169,7 +189,7 @@
         >
         <input
           name="Budget"
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
           type="text"
           bind:value={eventDetails.Budget}
         />
@@ -182,8 +202,9 @@
           >Contact Number</label
         >
         <input
+          placeholder="10 Digits Number"
           name="Number"
-          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
           type="text"
           bind:value={eventDetails.ContactNo}
         />
@@ -196,9 +217,10 @@
           >Message</label
         >
         <textarea
+          placeholder="Write your message here"
           bind:value={eventDetails.Message}
           name="message"
-          class="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          class="h-40 w-full rounded-md border-2 border-sky-400 bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
         />
       </div>
 
@@ -209,41 +231,33 @@
           >Send</button
         >
       </div>
-
-      <p class="text-xs text-gray-400">
-        By signing up to our newsletter you agree to our <a
-          href="#"
-          class="underline transition duration-100 hover:text-indigo-500 active:text-indigo-600"
-          >Privacy Policy</a
-        >.
-      </p>
     </form>
     <!-- form - end -->
   </div>
 </div>
 
 {#if cnt === 1}
-<div class="flex justify-center" >
-  <div
-    class="alert alert-info shadow-lg fixed top-10 w-1/2  justify-center"
-    in:fade
-    out:scale
-  >
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="stroke-current flex-shrink-0 w-6 h-6"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      <span>New software update available.</span>
+  <div class="flex justify-center">
+    <div
+      class="alert alert-info shadow-lg fixed top-10 w-1/2 justify-center"
+      in:fade
+      out:scale
+    >
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="stroke-current flex-shrink-0 w-6 h-6"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          /></svg
+        >
+        <span>New software update available.</span>
+      </div>
     </div>
   </div>
-</div>
 {/if}
--- src/store.js
+++ src/store.js
@@ -1,260 +1,263 @@
 import { writable } from "svelte/store";
 
-// Layout.svelte : 
+// Layout.svelte :
 import Logo from "$lib/calendar.png";
 export let layoutData = writable({
-    imgSrc: Logo,
-    title: "Event Hub",
-    navs: [
-        {
-            nv: "Home",
-            link: "/",
-        },
-        {
-            nv: "Events",
-            link: "/products",
-        },
-        {
-            nv: "About Us",
-            link: "/about",
-        },
-        {
-            nv: "Organize",
-            link: "/organize",
-        },
-    ],
+  imgSrc: Logo,
+  title: "Event Hub",
+  navs: [
+    {
+      nv: "Home",
+      link: "/",
+    },
+    {
+      nv: "Events",
+      link: "/products",
+    },
+    {
+      nv: "About Us",
+      link: "/about",
+    },
+    {
+      nv: "Organize",
+      link: "/organize",
+    },
+  ],
 });
-// Footer Page : 
+// Footer Page :
 export let footerData = writable({
-    footerTitle: "Event Hub",
-    footerLink: "funkytunky",
-    facebookLink: "/",
-    twitterLink: "/",
-    instagramLink: "/",
-    linkedinLink: "/",
+  footerTitle: "Event Hub",
+  footerLink: "eventhub",
+  facebookLink: "/",
+  twitterLink: "/",
+  instagramLink: "/",
+  linkedinLink: "/",
 });
-// Main page : 
+// Main page :
 export let mainPageData = writable([
-    {
-        id: 1,
-        metaTitle: "Event Management",
-        metaDesc: "Website Started Created by Sikandar Bhide",
-        metaImg: Logo
-    },
-    {
-        // +page.svelte data
-        id: 2,
-        title: "Welcome to Funky Tunky",
-        subtitle: "Classic Cars",
-        subtext: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!",
-        btn1: "Products",
-        btn2: "About",
+  {
+    id: 1,
+    metaTitle: "Event Management",
+    metaDesc: "Website Started Created by Sikandar Bhide",
+    metaImg: Logo,
+  },
+  {
+    // +page.svelte data
+    id: 2,
+    title: "Welcome to Event Hub",
+    subtitle: "",
+    subtext:
+      "Build & Organize High Quality Events, Our focus is to Create Auspicious Vibe for Customers",
+    btn1: "Events",
+    btn2: "About",
+  },
+  {
+    // +page.svelte data  <PageProd />
+    id: 3,
+    title: "Specials 💖",
+    img1: "https://img.freepik.com/free-photo/prepared-wedding-hall_8353-9873.jpg?w=996&t=st=1682279704~exp=1682280304~hmac=4c3460706d2cf6843f42e7f331b2f80f20f97064274ddc0dee3ebec811128379",
+    img2: "https://img.freepik.com/premium-photo/wedding-table-set-up-boho-style-with-pampas-grass-greenery-soft-focus_547542-1935.jpg?w=996",
+    btn: "View All",
+    text: "Organize Events as per needs, Recent Weddings & Party held in Lonavala",
+  },
+  {
+    // +page.svelte data  <ImgGallery />
+    id: 4,
+    img1: "https://img.freepik.com/free-photo/new-clean-luxury-restaurant-european-style-amara-dolce-vita-luxury-hotel-resort-tekirova-kemer-turkey_146671-18744.jpg?w=996&t=st=1682280281~exp=1682280881~hmac=5ade5f2320ed92a237b428866512a6aee49f624e01a3bb0785ca1a8a815f76c4",
+    img2: "https://img.freepik.com/free-photo/decorated-banquet-hall-with-flowers_8353-10058.jpg?w=996&t=st=1682280372~exp=1682280972~hmac=ee49d4cde6a8e7a0da2627931f897e66df9c7a88ff2f15bf39c8b69181afec54",
+    img3: "https://img.freepik.com/free-photo/ritual-with-coconut-leaves-during-traditional-hindu-wedding-ceremony_8353-8909.jpg?w=996&t=st=1682280468~exp=1682281068~hmac=f27e8e38966b25e9db357b5f6c148faaeea6896668e6bb431c09fb2128eed9b7",
+    img4: "https://img.freepik.com/free-photo/shine-wedding-altar-newlyweds-stands-backyard-decorated-with-balloons_8353-8415.jpg?w=996&t=st=1682280651~exp=1682281251~hmac=b9c470b4951f3d32b5f172419312f1d5e7ed7eff6a8661ee84aaf11d007270d4",
+    img1text: "Classic",
+    img2text: "Luxurious ",
+    img3text: "Successful Weddings",
+    img4text: "Romance",
+  },
+  {
+    // +page.svelte data   <FeaturesPage />
+    id: 5,
+    title: "Facilities & Services",
+    subtitle:
+      "Our Team Provide Valuable Servies to Our Customers, including Free Wi-fi, AC/Non-AC, Extended Timmings as per Need and  Parkings... ",
+    featuresDesc: [
+      {
+        title: "Free Wi-fi",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
+      },
 
-    },
-    {
-        // +page.svelte data  <PageProd />
-        id: 3,
-        title: "Vintage Cars",
-        img1: "https://i.pinimg.com/564x/d1/44/bc/d144bca97b033bdb904bf7c313cf2c93.jpg",
-        img2: "https://i.pinimg.com/564x/0a/8a/c0/0a8ac0cbaa1e69e295fb9ceb98deed99.jpg",
-        btn: "View All",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rerum quam amet provident nulla error!",
-    },
-    {
-        // +page.svelte data  <ImgGallery />
-        id: 4,
-        img1: "https://i.pinimg.com/564x/d2/ce/41/d2ce415a0159b22fc40ccc26d2ba623c.jpg",
-        img2: "https://images.pexels.com/photos/175712/pexels-photo-175712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        img3: "https://images.pexels.com/photos/12467508/pexels-photo-12467508.jpeg",
-        img4: "https://i.pinimg.com/564x/d2/76/08/d27608fa2f28fed73ae988d628911e18.jpg",
-        img1text: "Classic",
-        img2text: "",
-        img3text: "Retro",
-        img4text: "Retro",
-    },
-    {
-        // +page.svelte data   <FeaturesPage />
-        id: 5,
-        title: "Kickstart your Life or Die",
-        subtitle: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum.",
-        featuresDesc: [
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            },
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            },
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            },
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            },
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            },
-            {
-                title: "Digial Campaigns",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
-            }
-        ],
-        btnText: "Get Started Today",
-
-    },
-    {
-        // +page.svelte data   <FeaturesPage />
-        id: 6,
-        title: "Trusted by eCommerce Businesses",
-        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore provident impedit esse recusandae facere libero harum sequi.",
-        stats: [
-            {
-                title: "Total Sales",
-                value: "$4.8m",
-            },
-            {
-                title: "Official Addons",
-                value: "24",
-            },
-            {
-                title: "Total Addons",
-                value: "86",
-
-            },
-        ]
-    }
+      {
+        title: "Parking Servies ",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
+      },
+      {
+        title: "AC & Non AC",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
+      },
+      {
+        title: "8+ Cities",
+        description:
+          "Cities inlcuding : Mumbai, Navi Mumbai , Goa, Nashik, Banglore, Pune and future Cities : Kerla, Delhi, Odisha",
+      },
+      {
+        title: "Cash & Cash Less Payments",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
+      },
+      {
+        title: "Wonderful Decoration + Food",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio alias voluptatum blanditiis laudantium.",
+      },
+    ],
+    btnText: "Organize Events",
+  },
+  {
+    // +page.svelte data   <FeaturesPage />
+    id: 6,
+    title: "Trusted by Thousands",
+    subtitle:
+      "We are meant to Build Happiest & Succesful Events",
+    stats: [
+      {
+        title: "Total Events",
+        value: "3,300+",
+      },
+      {
+        title: "Prizes",
+        value: "60+",
+      },
+      {
+        title: "Cities",
+        value: "8+",
+      },
+    ],
+  },
 ]);
-// Product Page : 
-export let productsInfo = writable(
-    {
-        id: 1,
-        title: "Product Collections",
-        subtext: "lore ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
-        btn: "Buy Now", // for [Product Page]
-    }
-)
+// Product Page :
+export let productsInfo = writable({
+  id: 1,
+  title: "  Events",
+  subtext:
+    "",
+  btn: "Buy Now", // for [Product Page]
+});
 export let productData = writable([
-    {
-        id: 1,
-        title: "fifi",
-        brand: "Fancy Brand",
-        price: "$19.99",
-        oldprice: "$39.99",
-        img: "https://i.pinimg.com/564x/f7/d1/3a/f7d13acad6cb754438fd5a9da3d78818.jpg",
-        imgalt: "Photo by Austin Wade",
-    },
-    {
-        id: 2,
-        title: "Cool Outfit",
-        brand: "Cool Brand",
-        price: "$29.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/564x/20/83/06/208306ef7f5659a46c2b1c592e7851d2.jpg",
-    },
-    {
-        id: 3,
-        title: "Funky Outfit",
-        brand: "Funky Brand",
-        price: "$39.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/564x/f5/b3/b7/f5b3b7a64c253f882bfdd4337c7721ac.jpg",
-    },
-    {
-        id: 4,
-        title: "Tunky Outfit",
-        brand: "Tunky Brand",
-        price: "$49.99",
-        oldprice: "",
-        img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-        id: 5,
-        title: "Funky Outfit",
-        brand: "Funky Brand",
-        price: "$39.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/564x/a2/ea/98/a2ea989a1bc308ca8dfe9b2b30e93918.jpg",
-    },
-    {
-        id: 6,
-        title: "Tunky Outfit",
-        brand: "Tunky Brand",
-        price: "$49.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/564x/4d/d3/de/4dd3debfeaf25d0e935505eb81eb288f.jpg",
-    },
-    {
-        id: 7,
-        title: "Funky Outfit",
-        brand: "Funky Brand",
-        price: "$39.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/750x/da/1f/c9/da1fc9118f89831b25a0e5303350edc7.jpg",
-    },
-    {
-        id: 8,
-        title: "Tunky Outfit",
-        brand: "Tunky Brand",
-        price: "$49.99",
-        oldprice: "",
-        img: "https://i.pinimg.com/564x/05/fb/84/05fb8402f6b786dde6c69d09e545c8b0.jpg",
-    },
+  {
+    id: 1,
+    title: "fifi",
+    brand: "Fancy Brand",
+    price: "$19.99",
+    oldprice: "$39.99",
+    img: "https://i.pinimg.com/564x/f7/d1/3a/f7d13acad6cb754438fd5a9da3d78818.jpg",
+    imgalt: "Photo by Austin Wade",
+  },
+  {
+    id: 2,
+    title: "Cool Outfit",
+    brand: "Cool Brand",
+    price: "$29.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/564x/20/83/06/208306ef7f5659a46c2b1c592e7851d2.jpg",
+  },
+  {
+    id: 3,
+    title: "Funky Outfit",
+    brand: "Funky Brand",
+    price: "$39.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/564x/f5/b3/b7/f5b3b7a64c253f882bfdd4337c7721ac.jpg",
+  },
+  {
+    id: 4,
+    title: "Tunky Outfit",
+    brand: "Tunky Brand",
+    price: "$49.99",
+    oldprice: "",
+    img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 5,
+    title: "Funky Outfit",
+    brand: "Funky Brand",
+    price: "$39.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/564x/a2/ea/98/a2ea989a1bc308ca8dfe9b2b30e93918.jpg",
+  },
+  {
+    id: 6,
+    title: "Tunky Outfit",
+    brand: "Tunky Brand",
+    price: "$49.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/564x/4d/d3/de/4dd3debfeaf25d0e935505eb81eb288f.jpg",
+  },
+  {
+    id: 7,
+    title: "Funky Outfit",
+    brand: "Funky Brand",
+    price: "$39.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/750x/da/1f/c9/da1fc9118f89831b25a0e5303350edc7.jpg",
+  },
+  {
+    id: 8,
+    title: "Tunky Outfit",
+    brand: "Tunky Brand",
+    price: "$49.99",
+    oldprice: "",
+    img: "https://i.pinimg.com/564x/05/fb/84/05fb8402f6b786dde6c69d09e545c8b0.jpg",
+  },
 ]);
 
-// About Page : 
+// About Page :
 export let aboutInfo = writable([
-    {
-        id: 1,
-        smltitle: "Introducing",
-        title: "Revolutionary way to build the web",
-        subtitle: "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.",
-    },
-    {
-        id: 2,
-        title: "About us",
-        subtext: "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated",
-        liData: [
-            "This is a section of some simple filler text",
-            "Also known as placeholder text",
-            "It shares some characteristics of a real written text",
-        ],
-        quote: "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated",
-        abtImg: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600&h=350",
-    },
-    {
-        // Team Members : 
-        id: 3,
-        title: "Our Team by the numbers",
-        subtext: "We are a small team of passionate people who love what we do. We are always looking for new talent to join our team.",
-        stats: [
-            {
-                title: "100",
-                text: "People",
-            },
-            {
-                title: "500+",
-                text: "People",
-            },
-            {
-                title: "1000+",
-                text: "Customers",
-            },
-            {
-                title: "A couple",
-                text: "Coffee breaks",
-            },
-        ]
-
-
-    }
+  {
+    id: 1,
+    smltitle: "Introducing",
+    title: "Revolutionary way to build the web",
+    subtitle:
+      "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.",
+  },
+  {
+    id: 2,
+    title: "About us",
+    subtext:
+      "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated",
+    liData: [
+      "This is a section of some simple filler text",
+      "Also known as placeholder text",
+      "It shares some characteristics of a real written text",
+    ],
+    quote:
+      "This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated",
+    abtImg:
+      "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600&h=350",
+  },
+  {
+    // Team Members :
+    id: 3,
+    title: "Our Team by the numbers",
+    subtext:
+      "We are a small team of passionate people who love what we do. We are always looking for new talent to join our team.",
+    stats: [
+      {
+        title: "100",
+        text: "People",
+      },
+      {
+        title: "500+",
+        text: "People",
+      },
+      {
+        title: "1000+",
+        text: "Customers",
+      },
+      {
+        title: "A couple",
+        text: "Coffee breaks",
+      },
+    ],
+  },
 ]);
